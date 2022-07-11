9d8369aff8dc1e587c2b728b431f9827bf32b429
adel elmala <adel.elmala2025@gmail.com>
2022-07-11 15:14:14 +0200

    Time: 8 ms (6.90%), Space: 5.9 MB (25.59%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -4,14 +4,15 @@ public:
         int counter = 0;
         unsigned int uNum = (unsigned int) num;
         unsigned int LSBmask = 0x00000001;
+        unsigned int mask = 0xFFFFFFFE;
         while(uNum != 0)
         {
-            if((LSBmask & uNum) == 0) 
+            if((LSBmask & uNum) == 0) // even
             {
                 uNum >>= 1;
             }
-            else
-                uNum -=1;
+            else // odd
+                uNum &= mask;
             ++counter;
         }
         return counter;
